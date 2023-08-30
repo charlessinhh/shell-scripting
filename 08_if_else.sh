d4e9ccb0fa0612c23fdf2fd55e7c27bb212fed92
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-08-30 18:52:34 +0530

    if-else

--- 08_if_else.sh
+++ 08_if_else.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+
+#ask user and put in variable 
+read -p "Enter your marks: " marks
+
+#if-else
+if [[ $marks -gt 40 ]]
+    then
+        echo "You are passed"
+    else
+        echo "You are fail!!"
+fi
+
+
+#Elif 
+if [[ $marks -ge 80 ]]
+    then 
+        echo "First Division"
+elif [[ $marks -ge 60 ]]
+    then 
+        echo "Second Divison"
+elif [[ $marks -ge 40 ]]
+    then 
+        echo "Third Divison"
+else 
+    echo "Fail"
+fi
