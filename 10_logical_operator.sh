19ee9343ed11cba9d1d516fbe884d805a0565061
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-08-31 19:31:46 +0530

    logical

--- 10_logical_operator.sh
+++ 10_logical_operator.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+  
+#AND operator + OR operator 
+read -p "What is your age? " age
+read -p "what is your nationality? " nation
+
+if [[ $age -ge 18 ]] && [[ $nation == "india" || $nation == "India" || ${nation,,} == "india" ]]   #-eq for numeric and == for string 
+                                                                    # lower case nation
+    then
+        echo "You can vote"
+    else
+        echo "You can not vote"
+fi
+
+#cond1 && cond2 || cond3
+
+age = 18
+[[ age -ge 18 ]] && echo "Adult" || echo "Minor" 
