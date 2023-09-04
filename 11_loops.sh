8b29ba9baa9e91a168cfe958bc6719731bdb2e3c
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-09-04 19:39:41 +0530

    loop

--- 11_loops.sh
+++ 11_loops.sh
@@ -6,3 +6,21 @@ for i in 1 2 3 4 5
 do
     echo $i
 done
+
+#reading file and print item
+items = "/home/ubuntu/shell-learn/file.txt"
+
+for item in $(cat $items)
+do
+    echo $item
+done
+
+#array
+
+myArray=( 1 2 3 Hello Hi )
+length=${#myArray[*]}
+
+for (( i=0;i<$length;i++ ))  #double paranthesis
+do 
+    echo "Value of array is ${myArray[$i]}"
+done
