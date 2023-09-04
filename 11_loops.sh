bc615e66e6299482eeb4af4c492356f6e1f1f74e
CHARLES KUMAR SINGH <39948804+charlessinhh@users.noreply.github.com>
2023-09-04 19:48:09 +0530

    loops

--- 11_loops.sh
+++ 11_loops.sh
@@ -24,3 +24,33 @@ for (( i=0;i<$length;i++ ))  #double paranthesis
 do 
     echo "Value of array is ${myArray[$i]}"
 done
+
+
+
+# WHILE Loop
+
+count=0
+num=10
+
+while [ $count -le 10 ]
+do
+    echo "Numbers are $count"
+    let count++
+done
+
+#UNTIL loop
+
+a=10
+until [[ $a -eq 1 ]]
+do
+    echo "value of a is $a"
+    let a--
+done
+
+
+#INFINITE loop
+while true
+do 
+    echo "Hi"
+    sleep 2s
+done
